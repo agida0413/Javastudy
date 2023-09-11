5adf8c4b8308329344112268e2fcb30d1bc69066
yongj <yongj@192.168.0.107>
2023-09-11 15:19:49 +0900

    222234

---
+++ Chapter01_Project/src/자바언어_실행방법.java
@@ -0,0 +1,61 @@
+/*
+ * 
+ * 2page
+ * 자바 언어의 특징
+ * -----------------
+ * 1.실행방법
+ * =컴파일 방식 ==>2진 파일로 변경(컴퓨터가 인식하는 언어)
+ *	**자바의 특징 
+ *	컴파일 c/c++ =>운영체제마다 다른 컴파일을 한다
+ *				=>컴파일 된 내용이 다르다.
+ *		java = > 운영체제마다 동일
+ * 				컴파일된 파일 = 바이너리 코드
+ * 						----------
+ * 						one write
+ * 
+ * =인터프리터 방식 
+ * ==>한줄씩 번역해서 실행
+ * 	Html/css
+ *  
+ *  보안 = > 두번을 검색하기 때문에(소스 검증)
+ *  => 보안이 뛰어나다 : => 인증 시스템 
+ *  => 동원 증권 : 자바 => 시스템 구축
+ *  
+ *  		자바소스 
+ *  		JVM = >파일 (dll)
+ *		리눅스    윈도우   mac
+ * = 실행 과정 
+						기계어로 변경 ( javac)    한줄씩 번역(java)	
+ * 			A.java================= A.class================>Hello java
+ * 			프로그래머 코딩     |         바이너리 코드		|			
+ * 			  			 컴파일					 인터프리터
+ * 	=>편집기 : 이클립스, 인텔리j(파이참)
+ *          java+javac = ctrl+f11
+ * 			============어시스트
+ *         
+ *  	이클립스 다운로드 : eclipse.org = > J2EE
+ *  	
+ *  	6page : 개발환경 구축
+ *  			JDK : 17 (이상 버전은 한글 제어에 문제)
+ *  			-->스프링 (14)
+ *  				스프링 (STS =>3(Framework + boot) , STS  =>4(boot))
+ *  	=>jdk 환경설정
+ *  		1. path :  c:\jdk-17.0.2\bin
+ *  		2. classpath: .;c:\jdk-17.0.2\lib
+ *  
+ */ 
+public class 자바언어_실행방법 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello java");
+	System.out.println("Hello java");
+	System.out.println("Hello java");
+	System.out.println("Hello java");
+	System.out.println("Hello java");
+	
+		
+		
+}
+	
+}
