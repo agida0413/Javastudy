623c1817b51499d9082213559ba13f58495c9c0f
yongj <yongj@192.168.0.132>
2023-09-21 09:08:45 +0900

    111

---
+++ Chapter_03_제어문(반복문)_2/src/제어문_반복문5.java
@@ -0,0 +1,51 @@
+/*
+ *    ★   
+ *    ★★  
+ *    ★★★  
+ *    ★★★★  
+ *    
+ * 
+ * 
+ */
+
+import java.util.Iterator;
+
+public class 제어문_반복문5 {
+public static void main(String[] args) {
+//	for(int i=0;i<4;i++) {
+//		for(int j=0;j<i+1;j++) {
+//			
+//			System.out.print("★");
+//		}
+//		System.out.println();
+//		
+//	 	
+//		
+	
+//		
+	
+//for(int i =0 ; i<4; i++) {
+//		
+//		for(int j=0; j<4-i;j++) {
+//	System.out.print("★");		
+//	}
+//		System.out.println();
+	
+//	0 1   1 2  2  3
+	
+//}
+	
+	char c ='A';
+	
+	for(int i=0;i<4;i++) {
+		for(int j = 0;j<i+1;j++ ) {
+			
+			System.out.print(c++);
+			
+		
+		}
+		System.out.println();
+	}
+	
+}
+}
